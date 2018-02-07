96ef5493241392e4eef488a6750751c076eafdf0
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-06 19:04:02 -0500

    Initial fetch with error handling

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,37 @@
 import React, { Component } from 'react';
+const APIURL = 'https://todo-api-mernstack.herokuapp.com/api/todos';
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    }
+  }
+
+  componentWillMount(){
+    this.loadTodos();
+  }
+
+  loadTodos() {
+    fetch(APIURL)
+    .then(response => {
+      if(!response.ok) {
+        if(response.status >= 400 && response.status < 500) {
+          return response.json().then(data => {
+            let err = {errorMessage: data.message};
+            throw err;
+          })
+        } else {
+          let err = {errorMessage: 'Please try again later, server is not responding.'};
+          throw err;
+        }
+      }
+      return response.json();
+    })
+    .then(todos => this.setState({todos}));
+  }
+
   render() {
     return (
       <h1>Todo List</h1>
