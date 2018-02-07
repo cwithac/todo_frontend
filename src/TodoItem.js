59334a7cb89df4d83534ddfa02c2b6ef2cb92d54
Cathleen Wright <cathleenmwright@gmail.com>
2018-02-06 19:14:12 -0500

    Completed linethrough intial style

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const TodoItem = ({name}) => (
-  <li>{name}</li>
+const TodoItem = ({name, completed}) => (
+  <li
+    style={{
+      textDecoration: completed ? 'line-through' : 'none'
+    }}
+  >{name}</li>
 )
 
 export default TodoItem;
